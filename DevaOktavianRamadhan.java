214fd0782a808005e7f0e5a077827c086cdd283e
DevFlesvelka <devaoktavian66@gmail.com>
2025-09-19 20:11:01 +0700

    Add files via upload

---
+++ DevaOktavianRamadhan.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package devaoktavianramadhan;
+
+/**
+ *
+ * @author Asus
+ */
+public class DevaOktavianRamadhan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hai, Nama lengkap saya: Deva Oktavian Ramadhan");
+        System.out.println("Nama panggilan: Dev");
+        System.out.println("NRP: 5002251136");
+        System.out.println("Asal saya dari Lamongan");
+        System.out.println("Funfact saya sih sebenarnya cukup unik karena saya tidak menyukai musik yang mempunyai vokal sehingga saya cenderung lebih memilih mendengarkan musik instrumental");
+        System.out.println("Hobi, untuk hobi sebenarnya saya simpel saja dikarenakan hobi saya yaitu mendengarkan musik");
+        System.out.println("dan ada satu hal lagi yang menarik buat saya adalah musik yang saya dengarkan basanya adalah musik instrumental dengan genre hardcore dan juga fantasy folk");
+    }
+    
+}
